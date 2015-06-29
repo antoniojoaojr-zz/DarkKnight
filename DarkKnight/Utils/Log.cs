eb338af57ebc799dbc998d8c7689d7db429e48c4
antoniojoaojr <antonio@emplehstudios.com.br>
2015-06-29 16:17:47 -0300

    - Added tool server process - Added tool Logs

---
+++ DarkKnight/Utils/Log.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+#region License Information
+/* ************************************************************
+ * 
+ *    @author AntonioJr <antonio@emplehstudios.com.br>
+ *    @copyright 2015 Empleh Studios, Inc
+ * 
+ * 	  Project Folder: https://github.com/antoniojoaojr/DarkKnight
+ * 
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *    
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *    
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *    
+ * ************************************************************/
+#endregion
+
+namespace DarkKnight.Utils
+{
+    public enum LogLevel
+    {
+        WARNING,
+        ERROR,
+        TITLE,
+        TEXT
+    }
+    public class Log
+    {
+        private static bool started = false;
+
+        public static void Write(string msg)
+        {
+            Write(msg, LogLevel.TEXT);
+        }
+
+        public static void Write(string msg, LogLevel level)
+        {
+            if (!started)
+            {
+                started = true;
+                Write("Iniciado Logs");
+            }
+
+            string log = "";
+
+            switch (level)
+            {
+                case LogLevel.TITLE:
+                    log += "== [ " + DateTime.Now + " ] ";
+                    log = completeLine(log, '=');
+                    log += "== [ " + msg + " ] ";
+                    log = completeLine(log, '=');
+                    break;
+                case LogLevel.TEXT:
+                    log += "- [ " + DateTime.Now + " ] - " + msg;
+                    break;
+                case LogLevel.ERROR:
+                    log += "-- ERROR ";
+                    log = completeLine(log, '-');
+                    log += "- [ " + DateTime.Now + " ] - " + msg;
+                    log = completeLine(log, ' ');
+                    log += "-";
+                    log = completeLine(log, '-');
+                    break;
+                case LogLevel.WARNING:
+                    log += "-- WARNING ";
+                    log = completeLine(log, '-');
+                    log += "- [ " + DateTime.Now + " ] - " + msg;
+                    log = completeLine(log, ' ');
+                    log += "-";
+                    log = completeLine(log, '-');
+                    break;
+            }
+
+            Console.WriteLine(log);
+        }
+
+        private static string completeLine(string log, char complete)
+        {
+            int length = 80 - log.Length;
+            while (length < 0)
+                length += 80;
+
+            for (int i = 0; i < length; i++)
+                log += complete;
+
+            return log;
+        }
+    }
+}
---
+++ DarkKnight/core/ServerProcess.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Threading;
+
+#region License Information
+/* ************************************************************
+ * 
+ *    @author AntonioJr <antonio@emplehstudios.com.br>
+ *    @copyright 2015 Empleh Studios, Inc
+ * 
+ * 	  Project Folder: https://github.com/antoniojoaojr/DarkKnight
+ * 
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *    
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *    
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *    
+ * ************************************************************/
+#endregion
+
+namespace DarkKnight.core
+{
+
+    public delegate void ServerWork();
+
+    class ServerProcess
+    {
+        private ServerWork _work;
+        private int _delay;
+
+        public ServerProcess(ServerWork work, int delay)
+        {
+            _work = work;
+            _delay = delay;
+        }
+
+        public void _working()
+        {
+            while (ServerController.ServerRunning)
+            {
+                _work();
+
+                if (_delay > 0)
+                    Thread.Sleep(_delay);
+            }
+        }
+
+        /// <summary>
+        /// Run a function when the server is online with e delay
+        /// </summary>
+        /// <param name="work">DarkKnight.core.ServerWork delegation</param>
+        /// <param name="delay">int miliseconds</param>
+        public static void work(ServerWork work, int delay)
+        {
+            new Thread(new ThreadStart((new ServerProcess(work, delay)._working)));
+        }
+    }
+}
